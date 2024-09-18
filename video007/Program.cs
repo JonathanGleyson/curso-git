ee185d5fa70e1036ceecb4e7df811f45a546c046
Jonathan Gleyson <jonathangleyson@gmail.com>
2024-09-18 16:20:19 -0300

    Projeto iniciado, testando o git

---
+++ video007/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace video007 {
+    class Program {
+        static void Main(string[] args) {
+
+            DateTime d1 = DateTime.Now;
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d1.Ticks);
+            Console.WriteLine(d1.Day);
+
+            Console.ReadLine();
+        }
+    }
+}
